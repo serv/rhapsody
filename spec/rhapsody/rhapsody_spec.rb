8c8288e7fcd71341e26b6c7ed1af5379786076a7
Jason Kim <jasonkim@rhapsody.com>
2014-08-28 15:15:41 -0700

    finished v0.0.8

--- spec/rhapsody/rhapsody_spec.rb
+++ spec/rhapsody/rhapsody_spec.rb
@@ -13,7 +13,7 @@ describe Rhapsody do
   end
 
   it 'checks version' do
-    expect(Rhapsody::VERSION).to eql('0.0.8.beta3')
+    expect(Rhapsody::VERSION).to eql('0.0.8')
   end
 
   it 'has a helper for getting an access token' do
